c6b924cdd7db976ff0f38864ecfcbc049f37de37
Ed Colen <ed.colen@gmail.com>
2020-11-22 07:39:32 -0300

    created reviews methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,30 @@
 class ReviewsController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @review = Review.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @review = Review.new(review_params)
+    @review.cocktail = @cocktail
+    if @review.save
+      redirect_to @review.cocktail
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @cocktail = @review.cocktail
+    @review.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating, :cocktail_id)
+  end
 end
