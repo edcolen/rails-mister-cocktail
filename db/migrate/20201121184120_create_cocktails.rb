5f523c0e6178a568bb6ecffd1358849276de87c2
Ed Colen <ed.colen@gmail.com>
2020-11-23 10:57:44 -0300

    update

--- db/migrate/20201121184120_create_cocktails.rb
+++ db/migrate/20201121184120_create_cocktails.rb
@@ -2,6 +2,11 @@ class CreateCocktails < ActiveRecord::Migration[6.0]
   def change
     create_table :cocktails do |t|
       t.string :name
+      t.string :category
+      t.text :instructions
+      t.string :glass
+      t.boolean :alcoholic
+      t.boolean :mixed_by_user, default: false
 
       t.timestamps
     end
--- db/migrate/20201121185111_create_ingredients.rb
+++ db/migrate/20201121185111_create_ingredients.rb
@@ -2,6 +2,10 @@ class CreateIngredients < ActiveRecord::Migration[6.0]
   def change
     create_table :ingredients do |t|
       t.string :name
+      t.text :description
+      t.string :type
+      t.boolean :alcoholic
+      t.float :abv
 
       t.timestamps
     end
