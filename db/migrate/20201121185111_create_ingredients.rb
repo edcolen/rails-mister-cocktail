b52e7a59618dd23904a2c0d26996b5914f003837
Ed Colen <ed.colen@gmail.com>
2020-11-25 07:35:07 -0300

    added user reference

--- db/migrate/20201121185111_create_ingredients.rb
+++ db/migrate/20201121185111_create_ingredients.rb
@@ -1,6 +1,8 @@
 class CreateIngredients < ActiveRecord::Migration[6.0]
   def change
     create_table :ingredients do |t|
+      t.references :user, foreign_key: true
+
       t.string :name, null: false
       t.text :description, null: false
       t.string :type, null: false
