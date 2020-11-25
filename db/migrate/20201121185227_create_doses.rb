05543ebf6c675f7f21f22e3339f38b68649a23ea
Ed Colen <ed.colen@gmail.com>
2020-11-25 07:35:15 -0300

    organized

--- db/migrate/20201121185227_create_doses.rb
+++ db/migrate/20201121185227_create_doses.rb
@@ -3,6 +3,7 @@ class CreateDoses < ActiveRecord::Migration[6.0]
     create_table :doses do |t|
       t.references :cocktail, null: false, foreign_key: true
       t.references :ingredient, null: false, foreign_key: true
+
       t.string :measure
 
       t.timestamps
