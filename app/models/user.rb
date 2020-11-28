f3036a21007a136e4ee90e0dbcd5bcd73d6277e2
Ed Colen <ed.colen@gmail.com>
2020-11-28 08:06:51 -0300

    added user relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :cocktails, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  has_many :ingredients, dependent: :destroy
+
+  validates :username, :email, :password, presence: true
 end
