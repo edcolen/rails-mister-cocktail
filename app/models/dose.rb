70c499eb2a1c61fd75b58f9104387a4a72d5cd3c
Ed Colen <ed.colen@gmail.com>
2020-11-28 12:47:20 -0300

    created dose validations

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,6 +2,11 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
 
-  validates :description, :cocktail_id, :ingredient_id, presence: true
-  validates :cocktail_id, uniqueness: { scope: :ingredient_id }
+  validates :ingredient_id, presence: true
+  validates :cocktail_id, presence: true,
+                          uniqueness: { scope: :ingredient_id,
+                                        message: 'this cocktail already has this ingredient' }
+  validates :measure, presence: true,
+                      length: { minimum: 30,
+                                too_short: `measure should be at least %<count> characters long` }
 end
