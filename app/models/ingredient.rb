ca213952025eca6fc77a14c04f814e5de0ac6dc3
Ed Colen <ed.colen@gmail.com>
2020-11-28 14:53:52 -0300

    added validations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,16 @@
 class Ingredient < ApplicationRecord
+  belongs_to :user
   has_many :doses
 
-  validates :name, uniqueness: true, presence: true
+  has_one_attached :photo
+
+  validates :name, uniqueness: true,
+                   presence: true
+  validates :description, presence: true,
+                          length: { minimum: 30,
+                                    too_short: `description should be at least %<count> characters long` }
+  validates :type, presence: true
+  validates :alcoholic, presence: true
+  validates :abv, presence: true
+  validates :added_by_user, presence: true
 end
